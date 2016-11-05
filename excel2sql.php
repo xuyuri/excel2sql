f59560b5dc5e22c18da74ae5e03c25aad2b5422b
unknown <yurixu@yurixu-PC2.tencent.com>
2016-11-05 19:10:41 +0800

    update

--- excel2sql.php
+++ excel2sql.php
@@ -1,50 +1,87 @@
 <?php
-if ($_FILES["file"]["error"] > 0)
-  {
-  echo "Error: " . $_FILES["file"]["error"] . "<br />";
-  }
-else
-  {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-  echo "Type: " . $_FILES["file"]["type"] . "<br />";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
-  }
-?>
-    <script type="text/javascript">
-//        $("#sql").text("Hello world!");
-        alert(1);
-    </script>
-<?php
-
-  die;
-require_once 'Classes/PHPExcel/IOFactory.php';
+require_once 'resource/Classes/PHPExcel/IOFactory.php';
 //数据类型默认值
 $default = array(
-    'int' => '0', 
-    'varchar' => '', 
-    'tinyint' => '0',     
-    'text' => '', 
-    'smallint' => '0', 
-    'mediumint' => '0', 
-    'bigint' => '0', 
-    'float' => '0.0', 
-    'double' => '0.0', 
-    'decimal' => '0.0', 
-    'char' => '', 
-    'tinytext' => '', 
-    'mediumtext' => '', 
-    'longtext' => '',     
+    'int' => '0',
+    'varchar' => '',
+    'tinyint' => '0',
+    'text' => '',
+    'smallint' => '0',
+    'mediumint' => '0',
+    'bigint' => '0',
+    'float' => '0.0',
+    'double' => '0.0',
+    'decimal' => '0.0',
+    'char' => '',
+    'tinytext' => '',
+    'mediumtext' => '',
+    'longtext' => '',
 );
+//数据表默认字段列表
+$default_sql = "    `create_time` datetime NOT NULL COMMENT '创建时间',
+    `creator_wechat` varchar(50) NOT NULL DEFAULT '' COMMENT '创建者用户微信号',
+    `operate_time` datetime NOT NULL COMMENT '最后操作时间',
+    `operator_wechat` varchar(50) NOT NULL DEFAULT '' COMMENT '操作者用户微信',
+    `loginip` varchar(50) NOT NULL DEFAULT '' COMMENT '最后登录IP',
+    ";
+//整型字段列表
 $integer = ['int', 'tinyint', 'smallint', 'mediumint', 'bigint'];
-$file = './8.xls';
-//$html = '8.html';//excel2html($file);
-$html = excel2html($file);
-//echo $html."\n";die;
-parse($html);
-//int,varchar,tinyint,date,datetime,timestamp,text,smallint,mediumint,bigint,float,double,decimal,char,tinytext,mediumtext,longtext,date,time,year
+//转换结果存储文件
+$result_file = 'result.txt';
+$tmp_url = '';          //上传文件临时地址
+$html_url = '';         //转换完成的html文件地址
+$content = '';          //生成的sql语句集合
 
+main();
 
+/**
+ * Excel => Sql入口函数
+ * @author             yurixu 2016/11/2
+ */
+function main() {
+    global $tmp_url, $content, $result_file;
+    //上传文件检测
+    $init = initFile();
+    if($init) {
+        //Excel文件转Html
+        $html_url = excel2html($tmp_url);
+        if(!empty($html_url)) {
+            //Html文件解析
+            $content = html2sql($html_url);
+            @unlink($html_url);
+            if (!empty($content)) {
+                @file_put_contents($result_file, $content);
+            }
+        }
+    }
+}
+
+/**
+ * 上传文件检测
+ * @return bool
+ * @author             yurixu 2016/11/2
+ */
+function initFile() {
+    $result = false;
+    global $tmp_url, $content;
+    if ($_FILES["file"]["error"] > 0 ) {
+        $content = "Error: " . $_FILES["file"]["error"];
+    } else {
+        //文件类型：$_FILES["file"]["type"] 文件大小：$_FILES["file"]["size"] 文件名：$_FILES["file"]["name"] 文件存储位置：$_FILES["file"]["tmp_name"]
+        $tmp_url = $_FILES["file"]["tmp_name"];
+        $result = true;
+    }
+    return $result;
+}
+
+/**
+ * Excel文件转换为Html文件
+ * @param string $file      Excel文件地址
+ * @return string           转换为Html的文件地址
+ * @throws PHPExcel_Reader_Exception
+ * @throws PHPExcel_Writer_Exception
+ * @author                  yurixu 2016/11/2
+ */
 function excel2html($file='') {
     $result = '';
     if($file) {
@@ -63,13 +100,17 @@ function excel2html($file='') {
     return $result;
 }
 
-function parse($file='') {
-    global $default;
-    global $integer;
-    $default_column = 6;
+/**
+ * 解析Html文件内容，生成SQL语句
+ * @param string $file  html文件地址
+ * @return string       生成的sql内容字符串
+ * @author              yurixu 2016/11/2
+ */
+function html2sql($file='') {
+    global $default, $integer, $default_sql;
     $result = '';
-    if($file) {
-        $content = file_get_contents($file);
+    if(!empty($file)) {
+        $content = @file_get_contents($file);
         if($content) {
             $table_preg = '~<tr class="row\d+">
 			<td class="column0.*">表名</td>
@@ -95,23 +136,20 @@ function parse($file='') {
 			<td class="column4.*">.*</td>
 			<td class="column5.*">.*</td>
 		  </tr>\s*)*~';
-            preg_match_all($table_preg, $content, $table);      
-            //print_r($table);die;               
+            preg_match_all($table_preg, $content, $table);
             if($table && isset($table[0]) && isset($table[1]) && isset($table[2])) {
-                //echo json_encode($table);
-                $table_name = $table[1];
-                $table_engine = $table[2];
-                $table_comment = $table[3];
+                $table_name = $table[1];        //表名
+                $table_engine = $table[2];      //引擎类型
+                $table_comment = $table[3];     //表备注
                 $table = $table[0];
                 if($table) {
-                    $content = '';
+                    $result = '';
                     foreach($table as $k => $v) {
                         $start = "DROP TABLE IF EXISTS `$table_name[$k]`;\n";
                         $start .= "CREATE TABLE IF NOT EXISTS `$table_name[$k]` (\n"; 
                         $end = ") ENGINE = $table_engine[$k] DEFAULT CHARSET=utf8 COMMENT='$table_comment[$k]' ;";                    
                         $fields = array();       
                         $v = str_replace('&nbsp;', '', $v);
-                        //echo $v;die;                 
                         $field_preg = '~<tr class="row\d+">
 			<td class="column0.*">([^字表].*?)</td>
 			<td class="column1.*">(.*?)</td>
@@ -121,22 +159,24 @@ function parse($file='') {
 			<td class="column5.*">(.*?)</td>
 		  </tr>~';
                         preg_match_all($field_preg, $v, $fields);
-                        //print_r($fields);die;
                         if(!empty($fields)) {
                             $count = count($fields[0]);
                             $primary = 0;
                             for($i=0; $i<$count; $i++) {                                
-                                $column_name = $fields[1][$i];
-                                $column_type = $fields[2][$i];
-                                $column_len = $fields[3][$i];
-                                $column_default = $fields[4][$i];
-                                $column_comment = $fields[5][$i];
-                                $column_primary = $fields[6][$i];
+                                $column_name = $fields[1][$i];          //字段名
+                                $column_type = $fields[2][$i];          //字段类型
+                                $column_len = $fields[3][$i];           //字段长度
+                                $column_default = $fields[4][$i];       //字段默认值
+                                $column_comment = $fields[5][$i];       //字段备注
+                                $column_primary = $fields[6][$i];       //字段主键
                                 $column_default = $column_default ? $column_default : !empty($default[$column_type]) ? $default[$column_type] : ' ';
-                                $start .= "`$column_name` $column_type ($column_len) NOT NULL ";
+                                $start .= "    `$column_name` $column_type ($column_len) ";
+                                //整型自动设置为无符号类型unsigned
                                 if(in_array($column_type, $integer)) {
                                     $start .= 'unsigned ';
                                 }
+                                $start .= 'NOT NULL ';
+                                //主键自动设置为自增
                                 if($column_primary == 'Y') {
                                     $start .= 'AUTO_INCREMENT ';
                                     $primary = $i;
@@ -147,15 +187,14 @@ function parse($file='') {
                                 }
                                 $start .= "COMMENT '".$column_comment."',\n";
                             }
-                            $start .= "PRIMARY KEY (`".$fields[1][$primary]."`)\n";
-                            $content .= $start. $end."\n\n";
-                            //echo $content."\n"; 
+                            $start .= $default_sql;
+                            $start .= " PRIMARY KEY (`".$fields[1][$primary]."`)\n";
+                            $result .= $start. $end."\n\n";
                         }
                     }
                 }
-                echo $content;die;
-                
             }
         }
     }
+    return $result;
 }
--- index.php
+++ index.php
@@ -1,43 +1,53 @@
-<!--
-<html>
-<body>
-<form action="excel2sql.php" method="post" enctype="multipart/form-data">
-<label for="file">Filename:</label>
-<input type="file" name="file" id="file" /> 
-<br />
-<input type="submit" name="submit" value="Submit" />
-</form>
-</body>
-</html>
--->
-<?php
-
-?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 	<title>Music Download</title>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" media="all" href="style.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="resource/style.css" />
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
-    <!--<script src="dropzone.js"></script>
-    <script src="dropzone.css"></script>-->
-    <script src="dropzone.js"></script>
-    <link rel="stylesheet" href="dropzone.css">
+    <script src="resource/dropzone.js"></script>
+    <link rel="stylesheet" href="resource/dropzone.css">
 </head>
 <body>	
 	<div class="logo">Excel => SQL</div>
 		<div id="main">
 			<div class="form" >
-<!--                <form action="excel2sql.php" class="dropzone"></form>-->
-                <form action="excel2sql.php" class="dropzone"></form>
-			</div>
+                <form action="excel2sql.php" class="dropzone" id="upload"></form>
+            </div>
             <div >
                 <textarea class="result" rows="16" cols="50" id="sql"></textarea>
             </div>
 		</div>	
 </body>
+<div id="footer" align="center">
+    powered by yurixu
+</div>
 </html>
-<?php
+<script type="text/javascript">
+    function show() {
+        $.ajax({
+            async: false,
+            type: "GET",
+            url: "<?php echo 'result.txt'; ?>",
+            success: function (data) {
+                $("#sql").text(data);
+            }
+        });
+    }
+    //上传完成事件重写
+    $("#upload").dropzone({
+        success: function() {
+            show();
+        }
+    });
+    //----footer底部居中-----
+    var w=window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
 
-?>
+    var h=window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
+    document.getElementById("footer").style.width=w + "px";
+    //----footer底部居中-----
+</script>
